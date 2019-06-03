581d0c59992a66a94436c6e4ec3f2bd892861ac5
Hekmat Hamidi <theonehamidi@gmail.com>
2019-06-03 14:57:56 -0400

    finished q2

--- Lab04/Lab04.sql
+++ Lab04/Lab04.sql
@@ -6,3 +6,16 @@
  SELECT DEPARTMENT_NAME "Department Name", CITY "City", STREET_ADDRESS "Street Adress", POSTAL_CODE "Postal Code"
  FROM DEPARTMENTS JOIN LOCATIONS USING (LOCATION_ID)
  ORDER BY 1 DESC;
+
+/* 2. Display full name of the employees, their hire date and salary together with
+their department name, but only for departments which names start with A or S. Full name
+should be in format of :
+First / Last. Use the JOIN and ON form of syntax.
+Sort the output by department name and then by last name.
+*/
+
+
+SELECT FIRST_NAME || ' ' || LAST_NAME "Full Name", HIRE_DATE "Hire Date", SALARY "Salary", DEPARTMENT_NAME "Department Name" FROM EMPLOYEES T1
+INNER JOIN DEPARTMENTS T2 ON T1.DEPARTMENT_ID = T2.DEPARTMENT_ID
+WHERE DEPARTMENT_NAME LIKE 'A%' OR DEPARTMENT_NAME LIKE 'S%'
+ORDER BY "Department Name", T1.LAST_NAME;
