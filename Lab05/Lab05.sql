3b06ba002e8d17155cc6cd84e95176afbdd73833
Hekmat Wali Hamidi <hwhamidi@mtrx-node01pd.dcm.senecacollege.ca>
2019-06-04 17:51:01 -0400

    finished q3

--- Lab05/Lab05.sql
+++ Lab05/Lab05.sql
@@ -11,3 +11,14 @@ Sort the output by the most recent hire date.
 
 SELECT LAST_NAME "Last Name", JOB_ID "Job ID", HIRE_DATE "Hire Date" FROM EMPLOYEES
 WHERE HIRE_DATE > (SELECT HIRE_DATE FROM EMPLOYEES WHERE LAST_NAME = 'Grant');
+
+/* 3. Display city, province name and postal code for all departments located in countries that start with
+letter I (meaning Italy, Israel and India). If the province is blank, show message Unknown and the
+heading should be Province.
+Sort the output by city ascending. 
+*/
+
+SELECT CITY "City", STATE_PROVINCE || CASE WHEN STATE_PROVINCE IS NULL THEN 'Unknown' END "Province", POSTAL_CODE "Postal Code" FROM LOCATIONS
+WHERE COUNTRY_ID LIKE 'I%'
+ORDER BY 1;
+
