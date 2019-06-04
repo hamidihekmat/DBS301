62810993b2ade064fa8e76a5393621941f62af39
Hekmat Wali Hamidi <hwhamidi@mtrx-node01pd.dcm.senecacollege.ca>
2019-06-04 17:37:25 -0400

    finished q2

--- Lab05/Lab05.sql
+++ Lab05/Lab05.sql
@@ -5,3 +5,9 @@ from this query.
 SELECT LAST_NAME "Last Name", JOB_ID "Job ID" FROM EMPLOYEES WHERE JOB_ID = 
 (SELECT JOB_ID FROM EMPLOYEES WHERE LAST_NAME = 'Davies') AND LAST_NAME != 'Davies';
 
+/* 2. Display last name, job id and hire date for all employees hired after Grant.
+Sort the output by the most recent hire date.
+*/
+
+SELECT LAST_NAME "Last Name", JOB_ID "Job ID", HIRE_DATE "Hire Date" FROM EMPLOYEES
+WHERE HIRE_DATE > (SELECT HIRE_DATE FROM EMPLOYEES WHERE LAST_NAME = 'Grant');
