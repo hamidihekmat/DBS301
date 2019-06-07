693bd564bfa67a623b07a7fa74c11581dd9daba5
Hekmat Hamidi <theonehamidi@gmail.com>
2019-06-06 21:24:59 -0400

    finished q2

--- Assignment 1/DBS301_A1.sql
+++ Assignment 1/DBS301_A1.sql
@@ -20,3 +20,22 @@ WHERE TO_CHAR(HIRE_DATE, 'MM') = '05' OR TO_CHAR(HIRE_DATE, 'MM') = '11'
 AND TO_CHAR(HIRE_DATE, 'YYYY') NOT IN ('1994','1995')
 ORDER BY 4 DESC
 
+/* 2. List the employee number, full name, job and the modified salary for all
+employees whose monthly earning (without this increase) is outside the
+range $6,000  $11,000 and who are employed as Vice Presidents or
+Managers (President is not counted here).
+You should use Wild Card characters for this.
+VPs will get 30% and managers 20% salary increase.
+Sort the output by the top salaries (before this increase) firstly.
+Heading will be like Employees with increased Pay
+The output lines should look like this sample line:
+Emp# 124 : Kevin Mourgos is ST_MAN and will get a new salary of $6,960
+*/
+
+SELECT 'Emp# ' || EMPLOYEE_ID || ' : '  || FIRST_NAME || ' ' || LAST_NAME  || ' is ' || JOB_ID || ' and will get a new salary of $'  || 
+CASE WHEN JOB_ID LIKE '%MAN' THEN SALARY * 1.2 
+WHEN JOB_ID LIKE '%VP' THEN SALARY * 1.3
+WHEN JOB_ID LIKE '%MGR' THEN SALARY * 1.2 END "Employees with increased Pay" 
+FROM EMPLOYEES
+WHERE SALARY NOT BETWEEN 6000 AND 11000 AND JOB_ID LIKE '%MAN' OR JOB_ID LIKE '%VP' OR JOB_ID LIKE '%MGR'
+ORDER BY SALARY DESC;
